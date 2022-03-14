ac1958965ac93113d51db0e0283550f767f3dc95
Vladislav_Dmitriev <ironman751@yandex.ru>
2022-03-14 16:38:54 +0300

    attacking and taking damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,10 +7,16 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 10f;
+
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -19,7 +25,25 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                //Triggers Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        //Animation event
+        void Hit()
+        {
+            Health targetHealth = target.GetComponent<Health>();
+            targetHealth.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -37,5 +61,7 @@ namespace RPG.Combat
         {
             target = null;
         }
+
+        
     }
 }
